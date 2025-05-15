31bf0c90724613b445486f3f3f176ff00a13f22d
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-15 12:40:41 +0300

    Create class that handles login users

---
+++ src/main/java/app/user/service/MyUserDetailService.java
@@ -0,0 +1,29 @@
+package app.user.service;
+
+import app.user.model.User;
+import app.user.model.UserPrinciple;
+import app.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public MyUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User with username [%s] not found.".formatted(username)));
+
+        return new UserPrinciple(user);
+    }
+}
