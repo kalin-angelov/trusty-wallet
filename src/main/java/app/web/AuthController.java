a8a57464300258aed5c4f58169921340523dbb20
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-15 12:40:05 +0300

    Create controller for users

---
+++ src/main/java/app/web/AuthController.java
@@ -0,0 +1,47 @@
+package app.web;
+
+import app.user.service.UserService;
+import app.web.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AuthController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public ModelAndView getRegisterPage() {
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("register");
+        modelAndView.addObject("registerRequest", new RegisterRequest());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/register")
+    public ModelAndView registerUser(@Valid RegisterRequest registerRequest, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            return new ModelAndView("register");
+        }
+
+        userService.register(registerRequest);
+
+        return new ModelAndView("redirect:/login");
+    }
+
+
+}
