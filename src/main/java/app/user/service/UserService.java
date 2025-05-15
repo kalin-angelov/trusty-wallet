592e3b472ac03861bcc99350e669b0db92a9652b
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-15 12:43:10 +0300

    add some logic

--- src/main/java/app/user/service/UserService.java
+++ src/main/java/app/user/service/UserService.java
@@ -1,9 +1,63 @@
 package app.user.service;
 
+import app.user.model.User;
+import app.user.model.UserRole;
+import app.user.repository.UserRepository;
+import app.wallet.model.Wallet;
+import app.wallet.repository.WalletRepository;
+import app.wallet.service.WalletService;
+import app.web.dto.RegisterRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final WalletService walletService;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, WalletService walletService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.walletService = walletService;
+    }
+
+    public User register(RegisterRequest registerRequest) {
+
+        Optional<User> optionalUser = userRepository.findByUsername(registerRequest.getUsername());
+
+        if (optionalUser.isPresent()) {
+            log.info("User with username [%s] already exist.".formatted(registerRequest.getUsername()));
+        }
+
+        User user = userRepository.save(initializeUser(registerRequest));
+        Wallet wallet = walletService.createDefaultWallet(user);
+
+        user.setWallets(List.of(wallet));
+        userRepository.save(user);
+        log.info("User with id [%s] and username [%s] created successfully".formatted(user.getId(), user.getUsername()));
+        return user;
+    }
+
+    private User initializeUser (RegisterRequest registerRequest) {
+
+        return User.builder()
+                .username(registerRequest.getUsername())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(UserRole.USER)
+                .isActive(true)
+                .createdOn(LocalDateTime.now())
+                .build();
+    }
 }
--- src/main/java/app/wallet/service/WalletService.java
+++ src/main/java/app/wallet/service/WalletService.java
@@ -1,9 +1,45 @@
 package app.wallet.service;
 
+import app.user.model.User;
+import app.wallet.model.Wallet;
+import app.wallet.model.WalletStatus;
+import app.wallet.model.WalletType;
+import app.wallet.repository.WalletRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
 public class WalletService {
+
+    private final WalletRepository walletRepository;
+
+    @Autowired
+    public WalletService(WalletRepository walletRepository) {
+        this.walletRepository = walletRepository;
+    }
+
+    public Wallet createDefaultWallet(User user) {
+
+        Wallet wallet = initializeWallet(user);
+
+        log.info("Wallet it id [%s] successfully create for user with username [%s] and id [%s]".formatted(wallet.getId(), user.getUsername(), user.getId()));
+        return wallet;
+    }
+
+    private Wallet initializeWallet (User user) {
+
+        return Wallet.builder()
+                .owner(user)
+                .balance(new BigDecimal(0))
+                .type(WalletType.PAYABLE)
+                .status(WalletStatus.ACTIVE)
+                .createdOn(LocalDateTime.now())
+                .updatedOn(LocalDateTime.now())
+                .build();
+    }
 }
