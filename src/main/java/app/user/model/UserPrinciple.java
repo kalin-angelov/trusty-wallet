060ac53d1ab83caca2262ef34268ff3680f84a53
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-15 12:41:29 +0300

    Create class that handles the logged users

---
+++ src/main/java/app/user/model/UserPrinciple.java
@@ -0,0 +1,53 @@
+package app.user.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class UserPrinciple implements UserDetails {
+
+    private User user;
+
+    public UserPrinciple (User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return user.isActive();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isActive();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return user.isActive();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive();
+    }
+}
