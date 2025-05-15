100e74296fe5bb902d8105611174cf0541d4e190
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-15 12:42:21 +0300

    update

--- src/main/java/app/config/WebMvcConfig.java
+++ src/main/java/app/config/WebMvcConfig.java
@@ -1,13 +1,16 @@
 package app.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableMethodSecurity
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
@@ -15,6 +18,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         http
                 .authorizeHttpRequests(matchers -> matchers
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .requestMatchers("/", "/register").permitAll()
                         .anyRequest().authenticated()
                 )
