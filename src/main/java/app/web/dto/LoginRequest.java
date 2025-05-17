97c10e01022d12ee4295675fedc4bede9ce01339
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-17 10:53:21 +0300

    create DTO for login request

---
+++ src/main/java/app/web/dto/LoginRequest.java
@@ -0,0 +1,20 @@
+package app.web.dto;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    @Size(min = 4, message = "Username must be at least 4 characters long")
+    private String username;
+
+    @Size(min = 5, message = "Password must be at least 5 characters long")
+    private String password;
+}
