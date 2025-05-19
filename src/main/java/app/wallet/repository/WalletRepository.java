a69d0eedea49b1881a1c3406df69e86db6724c30
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-19 13:16:08 +0300

    update

--- src/main/java/app/wallet/repository/WalletRepository.java
+++ src/main/java/app/wallet/repository/WalletRepository.java
@@ -4,8 +4,10 @@ import app.wallet.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+    Optional<Wallet> findByIdAndOwnerId(UUID id, UUID ownerId);
 }
