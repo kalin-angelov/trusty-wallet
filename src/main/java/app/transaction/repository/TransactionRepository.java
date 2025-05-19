9e68f252f31b1d0d9d45523470031849b8c47cd6
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-19 13:16:33 +0300

    update

--- src/main/java/app/transaction/repository/TransactionRepository.java
+++ src/main/java/app/transaction/repository/TransactionRepository.java
@@ -4,8 +4,16 @@ import app.transaction.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+    List<Transaction> findAllTransactionByOwnerId(UUID ownerId);
+
+    List<Transaction> findAllBySenderOrReceiver(String sender, String receiver);
+
+    Optional<Transaction> findByIdAndOwnerId(UUID id, UUID ownerId);
 }
