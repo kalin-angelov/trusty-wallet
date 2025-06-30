85e95f64dc1697a8167e14fdc5c46a61fd9b1b04
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-06-30 16:49:13 +0300

    edit: add two more exception classes

--- src/main/java/app/web/ExceptionAdvice.java
+++ src/main/java/app/web/ExceptionAdvice.java
@@ -6,9 +6,12 @@ import app.user.model.UserPrinciple;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingRequestValueException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -20,7 +23,13 @@ import java.util.UUID;
 public class ExceptionAdvice {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({AccessDeniedException.class, NoResourceFoundException.class})
+    @ExceptionHandler({
+            AccessDeniedException.class,
+            NoResourceFoundException.class,
+            MethodArgumentTypeMismatchException.class,
+            MissingRequestValueException.class,
+            HttpRequestMethodNotSupportedException.class,
+    })
     public ModelAndView handleNotFoundExceptions (Exception exception) {
 
         return new ModelAndView("not-found");
