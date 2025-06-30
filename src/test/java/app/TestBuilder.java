09d46f194adf34e62e15e311bedae1398bb8a8cd
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-06-30 16:53:12 +0300

    create: class for dummy objects

---
+++ src/test/java/app/TestBuilder.java
@@ -0,0 +1,94 @@
+package app;
+
+import app.credit.model.Credit;
+import app.credit.model.CreditStatus;
+import app.user.model.User;
+import app.user.model.UserRole;
+import app.web.dto.EditRequest;
+import app.web.dto.LoginRequest;
+import app.web.dto.RegisterRequest;
+import lombok.experimental.UtilityClass;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@UtilityClass
+public class TestBuilder {
+
+    public static User aRandomUser () {
+
+        return User.builder()
+                .id(UUID.randomUUID())
+                .email("randomEmail@gmail.com")
+                .username("randomUsername")
+                .password("randomPassword")
+                .firstName("randomFirstName")
+                .lastName("randomLastName")
+                .profilePic("randomImgUrl")
+                .role(UserRole.USER)
+                .isActive(true)
+                .createdOn(LocalDateTime.now())
+                .updatedOn(LocalDateTime.now())
+                .build();
+    }
+
+    public static User aRandomAdmin () {
+
+        return User.builder()
+                .id(UUID.randomUUID())
+                .email("randomEmail@gmail.com")
+                .username("randomUsername")
+                .password("randomPassword")
+                .firstName("randomFirstName")
+                .lastName("randomLastName")
+                .profilePic("randomImgUrl")
+                .role(UserRole.ADMIN)
+                .isActive(true)
+                .createdOn(LocalDateTime.now())
+                .updatedOn(LocalDateTime.now())
+                .build();
+    }
+
+    public static Credit aRandomCredit () {
+
+        return Credit.builder()
+                .id(UUID.randomUUID())
+                .owner(aRandomUser())
+                .status(CreditStatus.PAYED)
+                .amount(new BigDecimal(0))
+                .payedOn(null)
+                .nextPaymentOn(LocalDate.now())
+                .build();
+    }
+
+    public static RegisterRequest aRandomRegisterRequest() {
+
+        return RegisterRequest.builder()
+                .email("email@gmail.com")
+                .username("username")
+                .password("password")
+                .build();
+
+    }
+
+    public static LoginRequest aRandomLoginRequest() {
+
+        return LoginRequest.builder()
+                .username("username")
+                .password("password")
+                .build();
+
+    }
+
+    public static EditRequest aRandomEditRequest() {
+
+        return EditRequest.builder()
+                .email("email@gmail.com")
+                .firstName("firstName")
+                .lastName("lastName")
+                .profilePic("imgUrl")
+                .build();
+    }
+}
