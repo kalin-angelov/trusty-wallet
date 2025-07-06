31c7de304c8f9d266c40a72e0f06963ef7e5a749
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-07-06 13:42:53 +0300

    edit: add view for the credit form

--- src/main/java/app/web/TransferController.java
+++ src/main/java/app/web/TransferController.java
@@ -1,10 +1,12 @@
 package app.web;
 
+import app.credit.service.CreditService;
 import app.transaction.model.Transaction;
 import app.user.model.User;
 import app.user.model.UserPrinciple;
 import app.user.service.UserService;
 import app.wallet.service.WalletService;
+import app.web.dto.CreditRequest;
 import app.web.dto.TransferRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +24,13 @@ public class TransferController {
 
     private final UserService userService;
     private final WalletService walletService;
+    private final CreditService creditService;
 
     @Autowired
-    public TransferController(UserService userService, WalletService walletService) {
+    public TransferController(UserService userService, WalletService walletService, CreditService creditService) {
         this.userService = userService;
         this.walletService = walletService;
+        this.creditService = creditService;
     }
 
     @GetMapping
@@ -46,6 +50,26 @@ public class TransferController {
 
         User user = userService.getUserById(userPrinciple.getUser().getId());
         Transaction transaction = walletService.makeTransfer(user, transferRequest);
+
+        return new ModelAndView("redirect:/transactions/" + transaction.getId());
+    }
+
+    @GetMapping("/credit")
+    public ModelAndView getPayCreditView(@AuthenticationPrincipal UserPrinciple userPrinciple) {
+        User user = userService.getUserById(userPrinciple.getUser().getId());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("credit");
+        modelAndView.addObject("creditRequest", new CreditRequest());
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+    @PostMapping("/credit")
+    public ModelAndView payCredit(@AuthenticationPrincipal UserPrinciple userPrinciple, @Valid CreditRequest creditRequest, BindingResult bindingResult) {
+        User user = userService.getUserById(userPrinciple.getUser().getId());
+
+        Transaction transaction = walletService.payCredit(user, creditRequest.getSender());
 
         return new ModelAndView("redirect:/transactions/" + transaction.getId());
     }
