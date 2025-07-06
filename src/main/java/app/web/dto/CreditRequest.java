aaf4a4d53e59bce522940a2ce10ee3683f5b8d17
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-07-06 12:34:45 +0300

    create: dto for credit request

---
+++ src/main/java/app/web/dto/CreditRequest.java
@@ -0,0 +1,19 @@
+package app.web.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditRequest {
+
+    @NotNull
+    private UUID sender;
+}
