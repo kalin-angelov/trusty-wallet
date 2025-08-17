ceace59faa6e02658911b0414c6f849985333f68
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-17 14:07:49 +0300

    edit: add

--- src/main/java/app/email/client/NotificationClient.java
+++ src/main/java/app/email/client/NotificationClient.java
@@ -1,8 +1,12 @@
 package app.email.client;
 
+import app.email.client.dto.NotificationPreferenceRequest;
+import app.email.client.dto.NotificationPreferenceResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
@@ -10,7 +14,10 @@ import java.util.UUID;
 @FeignClient(name = "notification-sender-app", url = "http://localhost:8081/api/v1/notifications")
 public interface NotificationClient {
 
+    @PostMapping("/preferences")
+    ResponseEntity<Void> upsertNotificationSetting(@RequestBody NotificationPreferenceRequest request);
+
     @GetMapping("/preferences")
-    ResponseEntity<String> getUserNotificationSetting(@RequestParam(name = "userId") UUID userId);
+    ResponseEntity<NotificationPreferenceResponse> getUserNotificationSetting(@RequestParam(name = "userId") UUID userId);
 
 }
