8ba1baa35cf47ba44a8e8bd80f3d238a0d2efb92
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-17 14:07:25 +0300

    edit: add methods for saving and getting notification setting for users

--- src/main/java/app/email/service/EmailService.java
+++ src/main/java/app/email/service/EmailService.java
@@ -1,9 +1,16 @@
 package app.email.service;
 
 import app.email.client.NotificationClient;
+import app.email.client.dto.NotificationPreferenceRequest;
+import app.email.client.dto.NotificationPreferenceResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Slf4j
 @Service
 public class EmailService {
     
@@ -11,4 +18,30 @@ public class EmailService {
     private NotificationClient notificationClient;
 
 
+    public void saveNotificationSetting(UUID userId, boolean isEmailEnable , String email) {
+
+        NotificationPreferenceRequest request = NotificationPreferenceRequest.builder()
+                .userId(userId)
+                .notificationEnabled(isEmailEnable)
+                .type("EMAIL")
+                .contactInfo(email)
+                .build();
+
+        ResponseEntity<Void> httpResponse = notificationClient.upsertNotificationSetting(request);
+
+        if (!httpResponse.getStatusCode().is2xxSuccessful()) {
+            log.error("[Feign call to notification-sender-app failed] Can't save notification settings for user with id - [%s]".formatted(userId));
+        }
+     }
+
+    public NotificationPreferenceResponse getNotificationSettings(UUID userId) {
+
+       ResponseEntity<NotificationPreferenceResponse> httpResponse = notificationClient.getUserNotificationSetting(userId);
+
+       if(!httpResponse.getStatusCode().is2xxSuccessful()) {
+            throw new RuntimeException("Notification setting for user with id - [%s] is not found".formatted(userId));
+       }
+
+       return httpResponse.getBody();
+    }
 }
