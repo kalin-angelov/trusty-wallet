d9a1b249a9e3e456ea17937036fa0d5059efd9e8
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-18 14:11:10 +0300

    edit: add end-point for activation and deactivation of notifications

--- src/main/java/app/web/NotificationController.java
+++ src/main/java/app/web/NotificationController.java
@@ -9,8 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/notifications")
@@ -35,5 +39,13 @@ public class NotificationController {
         modelAndView.addObject("notificationSetting", notificationSetting);
 
         return modelAndView;
+    }
+
+    @PutMapping("/{userId}/{enabled}")
+    public String changeNotificationSetting(@PathVariable UUID userId, @PathVariable boolean enabled) {
+
+        emailService.changeNotificationSetting(userId, !enabled);
+
+        return "redirect:/notifications";
     }
 }
