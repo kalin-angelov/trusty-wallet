28afafe30a21d3d6ca4b56dd87430f0baf290f1c
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-08-18 14:11:32 +0300

    edit: add end-point for activation and deactivation of notifications

--- src/main/java/app/email/client/NotificationClient.java
+++ src/main/java/app/email/client/NotificationClient.java
@@ -4,10 +4,7 @@ import app.email.client.dto.NotificationPreferenceRequest;
 import app.email.client.dto.NotificationPreferenceResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -20,4 +17,6 @@ public interface NotificationClient {
     @GetMapping("/preferences")
     ResponseEntity<NotificationPreferenceResponse> getUserNotificationSetting(@RequestParam(name = "userId") UUID userId);
 
+    @PutMapping("/preferences")
+    ResponseEntity<NotificationPreferenceResponse> changeSendingSetting(@RequestParam(name = "userId") UUID userId, @RequestParam(name = "enabled") boolean enabled);
 }
